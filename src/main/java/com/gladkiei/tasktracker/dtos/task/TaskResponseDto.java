db38df6dfc03fd12815a556fbca5fac9a473464e
EGladki <e.gladki@gmail.com>
2026-01-20 21:25:06 +0300

    ref: refactor TaskResponseDto by adding createdAt, refactor liquibase changelog

--- src/main/java/com/gladkiei/tasktracker/dtos/task/TaskResponseDto.java
+++ src/main/java/com/gladkiei/tasktracker/dtos/task/TaskResponseDto.java
@@ -4,15 +4,14 @@ import com.gladkiei.tasktracker.enums.TaskStatus;
 
 import java.sql.Timestamp;
 
-
 public record TaskResponseDto(
         Long id,
         String title,
         String description,
+        String createdAt,
         Timestamp completionTime,
         TaskStatus status,
         Long userId
 ) {
-
 
 }
